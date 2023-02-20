09656446e151840d5f511d9e6debe66fd043b0d8
sayalijadhav8931 <121795763+sayalijadhav8931@users.noreply.github.com>
2023-02-20 16:27:38 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
 #/bin/bash
-
-echo "Enter Something:"
-read something
-
-echo "You Entered:" $something
+clear
+echo "Please enter your name"
+read name
+echo "Please enter your age"
+read age
+echo "Please enter your Gender. Male/Female"
+read Gender
+echo "So you're a $age year old $Gender called $name"
